accde0751ed5612094c03295983db061fa715aed
celli33 <limon.martinez.misael@gmail.com>
2022-10-25 19:59:33 -0500

    fix: clean name of regimen 622

--- src/Regimen.php
+++ src/Regimen.php
@@ -113,7 +113,7 @@ class Regimen implements JsonSerializable
     public function setRegimen(string $regimen): void
     {
         $this->regimen = $regimen;
-        $regimenAsText = trim(str_replace(['Régimen de las', 'Régimen de los', 'Régimen de', 'Régimen'], '', $regimen));
+        $regimenAsText = trim(str_replace(['Régimen de las', 'Régimen de los', 'Régimen de', 'Régimen', 'PM'], '', $regimen));
         $this->regimenId = $this->searchRegimenIdByText($regimenAsText);
     }
 
