7ff26bf661e7df4aa7ad7ded38de55d9c9ddd2fb
Carlos C Soto <eclipxe13@gmail.com>
2025-11-10 13:14:29 -0600

    Remove duplicated test

--- tests/Unit/Persona/Traits/PersonaTrait.php
+++ tests/Unit/Persona/Traits/PersonaTrait.php
@@ -31,30 +31,13 @@ trait PersonaTrait
     }
 
     /**
-    *
-    * test to check if new property not contained in Persona is found is assigned and value can be retrieved
-    * its goal is not to be used but to prevent unexpected exceptions
-    * */
+     * test to check if new property not contained in Persona is found is assigned and value can be retrieved
+     * its goal is not to be used but to prevent unexpected exceptions
+     */
     public function test_assign_dynamic_property(): void
     {
         /** @phpstan-ignore-next-line */
         $this->person->{'dynamic'} = 'dynamic';
         $this->assertSame('dynamic', $this->person->{'dynamic'});
-    }
-
-    public function test_allows_more_than_one_regimen(): void
-    {
-        $first = new Regimen();
-        $first->setRegimen('Simplificado de confianza');
-        $second = new Regimen();
-        $second->setRegimen('Demás ingresos');
-
-        $person = $this->person;
-        $person->addRegimenes($first, $second);
-
-        $this->assertSame(
-            [$first, $second],
-            $person->getRegimenes()->getRegimenes(),
-        );
     }
 }
